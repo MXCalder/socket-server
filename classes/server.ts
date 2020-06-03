25afef7e49775145ef953b902ad57887555c784d
Critóbal Calderón <cristobal.calderon.g@gmail.com>
2020-06-03 02:29:34 -0500

    Se configuran variables io y se agrega http en lugar de express

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,34 @@
 import express = require('express');
 import { SERVER_PORT } from "../global/environment";
-import socketIO from 'socket.io';
+import socketIO = require('socket.io');
+import http = require("http");
 
 export default class Server {
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
     constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
     }
 
     start( callback: VoidFunction ) {
-        this.app.listen( this.port, callback );
+        // this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
+    }
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on("connection", cliente =>{
+           console.log('Cliente conectado');
+        });
     }
 }
--- dist/Classes/server.js
+++ dist/Classes/server.js
@@ -2,13 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var environment_1 = require("../global/environment");
+var socketIO = require("socket.io");
+var http = require("http");
 var Server = (function () {
     function Server() {
         this.app = express();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        this.escucharSockets();
     }
     Server.prototype.start = function (callback) {
-        this.app.listen(this.port, callback);
+        // this.app.listen( this.port, callback );
+        this.httpServer.listen(this.port, callback);
+    };
+    Server.prototype.escucharSockets = function () {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on("connection", function (cliente) {
+            console.log('Cliente conectado');
+        });
     };
     return Server;
 }());
